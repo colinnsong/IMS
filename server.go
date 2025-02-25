e475b210c7ebe52f9ceec7af41f133c3a9801843
colin <1322544157@qq.com>
2025-02-25 10:57:02 +0800

    V0.3用户消息广播功能实现

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -30,6 +31,26 @@ func (server *Server) Handler(conn net.Conn) {
 
 	// 广播用户上线的消息
 	server.BroadCast(user, "已上线")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				server.BroadCast(user, "已下线")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("conn.Read err:", err)
+				return
+			}
+
+			msg := string(buf[:n-1])
+			server.BroadCast(user, msg)
+		}
+	}()
 
 	// 保证当前go程不睡眠
 	select {}
